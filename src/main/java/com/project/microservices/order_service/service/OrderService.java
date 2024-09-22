0fec816921a8834b03d65c2c8d9c5541295f1602
BENMOUSSA SALMA <salma.benmoudssa@gmail.com>
2024-09-22 17:03:48 +0100

    Add Spring cloud OpenFeign & Testing with Wiremock

--- src/main/java/com/project/microservices/order_service/service/OrderService.java
+++ src/main/java/com/project/microservices/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.project.microservices.order_service.service;
 
+import com.project.microservices.order_service.client.InventoryClient;
 import com.project.microservices.order_service.dto.OrderRequest;
 import com.project.microservices.order_service.model.Order;
 import com.project.microservices.order_service.repository.OrderRepository;
@@ -14,15 +15,24 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private InventoryClient inventoryClient;
+
     public void placeOrder(OrderRequest orderRequest){
 
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
 
-        orderRepository.save(order);
+        if(isProductInStock){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        }else{
+            throw new RuntimeException("Product with Skucode"+ orderRequest.skuCode() + "is not in stock");
+        }
+
 
     }
 }
--- src/test/java/com/project/microservices/order_service/OrderServiceApplicationTests.java
+++ src/test/java/com/project/microservices/order_service/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.project.microservices.order_service;
 
+import com.project.microservices.order_service.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
@@ -8,12 +9,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 import static  org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -35,11 +38,13 @@ class OrderServiceApplicationTests {
 	void shouldSubmitOrder() {
 		String submitOrderJson = """
 				{
-				    "skuCode": "iPhon_15",
+				    "skuCode": "iphone_15",
 				    "price": 1000,
-				    "quantity": 1
+				    "quantity": 100
 				}
 				""";
+
+		InventoryClientStub.stubInventoryCall("iphone_15", 100);
 
 		var responseBodyString = RestAssured.given()
 				.contentType("application/json")
