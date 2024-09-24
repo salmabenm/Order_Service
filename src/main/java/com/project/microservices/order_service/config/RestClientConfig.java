63a2f68af5c35f1a115580876959fdfa6931461a
BENMOUSSA SALMA <salma.benmoudssa@gmail.com>
2024-09-24 17:39:10 +0100

    REST Clients

---
+++ src/main/java/com/project/microservices/order_service/config/RestClientConfig.java
@@ -0,0 +1,27 @@
+package com.project.microservices.order_service.config;
+
+import com.project.microservices.order_service.client.InventoryClient;
+import org.apache.http.protocol.HttpService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${inventory.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public InventoryClient inventoryClient(){
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+}
